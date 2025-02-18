0da6fb062d88fcff786f85a71676fd37c0a24080
ReindorfN <144750275+ReindorfN@users.noreply.github.com>
2025-02-18 12:16:50 +0000

    update

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:contact_management_app/screens/about.dart';
 import "package:flutter/material.dart";
 import 'package:contact_management_app/screens/add_contact.dart';
 import 'package:contact_management_app/screens/contact_list.dart';
-import 'package:contact_management_app/screens/edit_contact.dart';
+// import 'package:contact_management_app/screens/edit_contact.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,77 +14,76 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) => MaterialApp(
-    title: 'Contact Management',
-    home: Scaffold(
-      appBar: AppBar(
-        title: const Text("Contact Management"),
-        backgroundColor: Colors.purpleAccent,
-      ),
-      drawer: const NavigationDrawer(),
-    ),
-  );
+        title: 'Contact Management',
+        home: Scaffold(
+          appBar: AppBar(
+            title: const Text("Contact Management"),
+            backgroundColor: Colors.purpleAccent,
+          ),
+          drawer: const NavigationDrawer(),
+        ),
+      );
 }
 
-
 //Navigation drawer class
-class NavigationDrawer extends StatelessWidget{
+class NavigationDrawer extends StatelessWidget {
   const NavigationDrawer({super.key});
 
   @override
   Widget build(BuildContext context) => Drawer(
-    child: SingleChildScrollView(
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: <Widget>[
-          buildHeader(context),
-          buildMenuItems(context),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              buildHeader(context),
+              buildMenuItems(context),
+            ],
+          ),
+        ),
+      );
 
-        ],
-      ),
-    ),
-  );
-
-  Widget buildHeader(BuildContext  context) => Container(
-    padding: EdgeInsets.only(
-      top: MediaQuery.of(context).padding.top,
-    ),
-  );
+  Widget buildHeader(BuildContext context) => Container(
+        padding: EdgeInsets.only(
+          top: MediaQuery.of(context).padding.top,
+        ),
+      );
 
   Widget buildMenuItems(BuildContext context) => Container(
-    padding: const EdgeInsets.all(24),
-    child: Wrap(
-    runSpacing: 16,
-    children: [
-      ListTile(
-        leading: const Icon(Icons.contacts),
-        title: const Text("Contacts"),
-        onTap: () => 
-        Navigator.of(context).pushReplacement(MaterialPageRoute(
-          builder: (context) => const ContactsPage(),
-          )),
-      ),
+        padding: const EdgeInsets.all(24),
+        child: Wrap(
+          runSpacing: 16,
+          children: [
+            ListTile(
+              leading: const Icon(Icons.contacts),
+              title: const Text("Contacts"),
+              onTap: () =>
+                  Navigator.of(context).pushReplacement(MaterialPageRoute(
+                builder: (context) => const ContactsPage(),
+              )),
+            ),
 
-    //Divider
-      const Divider(color: Colors.black54,),
+            //Divider
+            const Divider(
+              color: Colors.black54,
+            ),
 
-      ListTile(
-        leading: const Icon(Icons.add),
-        title: const Text("Add Contact"),
-        onTap: () => 
-        Navigator.of(context).pushReplacement(MaterialPageRoute(
-          builder: (context) => const AddContact(),
-          )),
-      ),
-      ListTile(
-        leading: const Icon(Icons.info),
-        title: const Text("About"),
-        onTap: () => 
-        Navigator.of(context).pushReplacement(MaterialPageRoute(
-          builder: (context) => const AboutPage(),
-          )),
-      ),
-      
-    ],
-  ),
-  );
+            ListTile(
+              leading: const Icon(Icons.add),
+              title: const Text("Add Contact"),
+              onTap: () =>
+                  Navigator.of(context).pushReplacement(MaterialPageRoute(
+                builder: (context) => const AddContact(),
+              )),
+            ),
+            ListTile(
+              leading: const Icon(Icons.info),
+              title: const Text("About"),
+              onTap: () =>
+                  Navigator.of(context).pushReplacement(MaterialPageRoute(
+                builder: (context) => const AboutPage(),
+              )),
+            ),
+          ],
+        ),
+      );
 }
